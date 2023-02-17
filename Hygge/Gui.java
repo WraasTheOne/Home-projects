411bb3e505eda0c3e370abd22385d86621f11033
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2023-02-17 13:20:12 +0100

    hej

--- Hygge/Gui.java
+++ Hygge/Gui.java
@@ -1,28 +1,17 @@
 package Hygge;
 
 import javax.swing.*;
-import java.awt.*;
 
-public class Gui extends JPanel{
+public class Gui extends JFrame{
+    private JPanel mainPanel;
 
-    JPasswordField passwordField1;
-    JPanel panel1;
-    JTextField textField1;
-    private JButton button1;
+    public Gui(String tittle){
+        super(tittle);
 
-    public void frame(){
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420,420);
-        frame.setVisible(true);
-        panel1 = new JPanel();
-
-        textField1 = new JTextField();
-        textField1.setBounds(180,180,100,20);
-
-        frame.add(button1);
-        System.out.println(button1.isShowing());
-        frame.add(textField1);
-
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+        this.setSize(420,420);
+        this.setVisible(true);
     }
 }
--- Hygge/Helloworld.java
+++ Hygge/Helloworld.java
@@ -1,13 +1,9 @@
 package Hygge;
-
 import javax.swing.*;
+
 
 class Helloworld{
     public static void main(String[] args){
-        Gui frame = new Gui();
-        frame.frame();
-
-        String hi = "hi";
-        System.out.println(hi);
+        Gui frame = new Gui("Hello world with Gui");
     }
 }
